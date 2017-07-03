fc08b4ce69c030a8eb563e6a3996b14aa3973d41
kfili <kosta.filiotis@gmail.com>
2017-07-03 12:28:48 +0300

    Started solution to sum-of-positive

---
+++ sum-of-positive/problem.rb
@@ -0,0 +1,11 @@
+# https://www.codewars.com/kata/sum-of-positive/train/ruby
+
+# You get an array of numbers, return the sum of all of the positives ones.
+
+# Example [1,-4,7,12] => 1 + 7 + 12 = 20
+
+# Note: array may be empty, in this case return 0.
+
+def positive_sum(arr)
+  # your code here
+end
---
+++ sum-of-positive/solution.rb
@@ -0,0 +1,8 @@
+def positive_sum(arr)
+  $total
+  while $i <= arr.length
+    if arr[$i] > 0
+      $total += arr[$i]
+    end
+  end
+end
