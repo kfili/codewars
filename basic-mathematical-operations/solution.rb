0823866fc349f01edd28d1790b54496674d603d9
kfili <kosta.filiotis@gmail.com>
2017-08-01 14:18:34 -0400

    Complete problem: Basic Mathematical Operations

---
+++ basic-mathematical-operations/solution.rb
@@ -0,0 +1,11 @@
+def basic_op(operator, value1, value2)
+  if operator == '+'
+    value1 + value2
+  elsif operator == '-'
+    value1 - value2
+  elsif operator == '*'
+    value1 * value2
+  elsif operator == '/'
+    value1 / value2
+  end
+end
