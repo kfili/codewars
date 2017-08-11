03717a9696b72eb19e62a8b9ad7dd2e32b3f68d8
kfili <kosta.filiotis@gmail.com>
2017-08-11 01:47:29 -0400

    Started 'Mumbling'

---
+++ 7kyu-difficulty-level/mumbling/solution.rb
@@ -0,0 +1,9 @@
+def accum(s)
+  result = ""
+  result << "#{s.upcase}-#{s + "-"}"
+#   counter = 0
+#   s.chars.each do |char|
+#     result << "#{char.upcase}-#{counter * ( char + "-" )}"
+#   end
+#   result
+end
