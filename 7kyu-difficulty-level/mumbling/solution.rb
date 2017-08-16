bdac1714f4d641d2d54fc9473e4fc1ec2f073ea5
kfili <kosta.filiotis@gmail.com>
2017-08-15 23:53:44 -0400

    Complete solution to 'Mumbling'

--- 7kyu-difficulty-level/mumbling/solution.rb
+++ 7kyu-difficulty-level/mumbling/solution.rb
@@ -1,9 +1,8 @@
 def accum(s)
   result = ""
-#   result << "#{s.upcase}" + "s" * i
   counter = 0
   s.chars.each do |char|
-    result << "#{char.upcase}" + "#{char}" * counter + "-"
+    result << "#{char.upcase}" + "#{char.downcase}" * counter + "-"
     counter += 1
   end
   result[0...-1]
