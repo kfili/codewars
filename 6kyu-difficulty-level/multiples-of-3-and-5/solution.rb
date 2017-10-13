2df9612417ad021343d38e8cdf83bcd93fa5b1bd
kfili <kosta.filiotis@gmail.com>
2017-10-12 22:31:27 -0400

    Complete problem 'Multiples of 3 and 5' in Ruby

--- 6kyu-difficulty-level/multiples-of-3-and-5/solution.rb
+++ 6kyu-difficulty-level/multiples-of-3-and-5/solution.rb
@@ -1,3 +1,9 @@
 def solution(number)
- # put your solution here
+  arr = [0]
+  (3..number - 1).each do |n|
+    if (n % 3).zero? || (n % 5).zero?
+      arr << n
+    end
+  end
+  arr.reduce(0, :+)
 end
