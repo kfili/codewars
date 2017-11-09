2c3a4b88de654bb4ca5ba87b77944ccfd57348ef
kfili <kosta.filiotis@gmail.com>
2017-11-09 00:00:27 -0500

    Complete problem 'Growth of a Population' in Ruby

--- 7kyu-difficulty-level/growth-of-a-population/solution.rb
+++ 7kyu-difficulty-level/growth-of-a-population/solution.rb
@@ -1,4 +1,4 @@
 def nb_year(p0, percent, aug, p)
-  p_x = p0 * (1 + percent * 0.01) + aug
-  p_x > p ? 0 : 1 + nb_year(p_x, percent, aug, p)
+  p_x = (p0 * (1 + percent * 0.01) + aug).to_i
+  p_x >= p ? 1 : 1 + nb_year(p_x, percent, aug, p)
 end
